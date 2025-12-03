a8bf970d56003295f15acce706fc528d17e62ca5
everviera12 <evertigre10@gmail.com>
2025-12-03 10:26:15 -0600

    refactor: enable conditional limiter and trust proxy configuration for production

--- src/server.js
+++ src/server.js
@@ -5,34 +5,40 @@ const app = express()
 const providers = require('./routes/provider.route');
 
 // middlewares
-/* const { conditionalLimiter } = require('./middleware/ratelimit'); */
+const { conditionalLimiter } = require('./middleware/ratelimit');
 
 app.use(express.json());
 
 /** Proxy config
 * Enable trust proxy to correctly read client IPs when behind a reverse proxy.
 */
-/* if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.set("trust proxy", true);
-} */
+}
 
 /** API base route
  *  Applies rate limiting ONLY when running in production.
  *  @conditionalLimiter - function to handle many request
  *  @providers - route to show all provider data
 */
-app.use('/api/v1/providers', providers);
+app.use('/api/v1/providers', conditionalLimiter, providers);
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>Welcome to my Express API. Visit 
+            <a href='https://victorsweb.site' target='_blank' rel='noopener noreferrer'>victorsweb.site</a> 
+            to see my work.
+        </h1>
+    `)
+});
 
 app.get("/ping", (req, res) => {
     res.json({ ok: true, timestamp: new Date() });
 });
 
 
-/* https://express-api-v1.up.railway.app */
 const port = process.env.PORT || 3000;
-
 console.log("NODE_ENV:", process.env.NODE_ENV);
-console.log("PORT:", process.env.PORT);
 console.log("SUPABASE_URL:", process.env.SUPABASE_URL ? "set" : "missing");
 console.log("SUPABASE_KEY:", process.env.SUPABASE_KEY ? "set" : "missing");
 app.listen(port, () => {
